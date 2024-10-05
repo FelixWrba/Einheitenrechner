debe4ffbf6325954dbbee7494a353e02ab86d0cf
Felix Wrba <felix.wrba@web.de>
2024-10-05 12:45:25 +0200

    Fist Commit!

---
+++ README.md
@@ -0,0 +1,6 @@
+# Einheitenrechner
+## Beschreibung
+Mit unserem Einheitenrechner können Sie schnell und einfach zwischen verschiedenen Maßeinheiten umrechnen. Ob Längen, Währungen, Volumen, Temperaturen oder mehr - die App bietet eine benutzerfreundliche Oberfläche und unterstützt zahlreiche gängige Einheiten. Ideal für den Alltag, die Arbeit oder beim Reisen, ermöglicht der Umrechner präzise Ergebnisse in Sekundenschnelle.
+## Nutzung
+Du kannst die Webapp unter folgender Webadresse erreichen: (Einheitenrechner.netlify.app)[https://einheitenrechner.netlify.app/]
+## Installation
---
+++ script.js
@@ -0,0 +1,48 @@
+document.getElementById('year').innerHTML = new Date().getFullYear();
+
+class Unit {
+    example;
+    constructor(example) {
+        this.example = example;
+    }
+}
+
+let main = document.getElementById('main');
+let win = {
+    window: document.getElementById('window'),
+    header: document.getElementById('header-win'),
+    main: document.getElementById('main-win')
+}
+let units = {
+    'Zeit': new Unit('120s'),
+    'Länge': new Unit('20m'),
+    'Fläche': new Unit('16m2'),
+    'Volumen': new Unit('4cm3'),
+    'Masse': new Unit('8kg'),
+    'Temperatur': new Unit('18F'),
+    'Währung': new Unit('12€')
+}
+
+function openUnit(unit) {
+    win.header.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff" onclick="goBack();"><path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/></svg><h1>${unit}</h1>`;
+    document.getElementById('win-input').innerHTML = `<input type="text" class="input" placeholder="z. B. ${units[unit].example}"><button class="btn-pink">Berechnen</button>`;
+    // let html = '';
+    // win.main.innerHTML = html;
+
+    win.window.style.top = '0%';
+    win.window.style.opacity = '1';
+}
+
+function goBack() {
+    let html = '<p>Einheit zum Berechnen anklicken...</p><ul id="unit-list">';
+    for (key in units) {
+        html += `<li onclick="openUnit('${key}')" role="button">${key}</li>`;
+    }
+    html += '</ul>';
+    document.getElementById('header').scrollIntoView({ 'behavior': 'smooth' });
+    main.innerHTML = html;
+    win.window.style.top = '110%';
+    win.window.style.opacity = '0';
+}
+
+openUnit("Länge");
