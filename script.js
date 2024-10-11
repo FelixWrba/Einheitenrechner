d059b060ffa84592936fff3bf7b2a01d67018632
Felix Wrba <felix.wrba@web.de>
2024-10-11 11:23:42 +0200

    Added functionality for units [10% done]

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 class Unit {
     example;
+    calculate;
     constructor(example, calculate) {
         this.example = example;
         this.calculate = calculate;
@@ -15,18 +16,37 @@ let win = {
 }
 
 let units = {
-    'Zeit': new Unit('120s'),
-    'Länge': new Unit('20m'),
-    'Fläche': new Unit('16m2'),
-    'Volumen': new Unit('4cm3'),
-    'Masse': new Unit('8kg'),
-    'Temperatur': new Unit('18F'),
-    'Währung': new Unit('12€')
+    'Zeit': new Unit('120s', (value, unit) => {
+        let seconds = value;
+        return `<p>Sekunden: ${seconds}</p>
+        <p>Minuten: ${seconds / 60}</p>
+        <p>Stunden: ${seconds / 3600}</p>
+        <p>Tage: ${seconds / 86400}</p>
+        <p>Jahre: ${seconds / 31536000}</p>`;
+    }),
+    'Länge': new Unit('20m', (value, unit) => {
+
+    }),
+    'Fläche': new Unit('16m2', (value, unit) => {
+
+    }),
+    'Volumen': new Unit('4cm3', (value, unit) => {
+
+    }),
+    'Masse': new Unit('8kg', (value, unit) => {
+
+    }),
+    'Temperatur': new Unit('18F', (value, unit) => {
+
+    }),
+    'Währung': new Unit('12€', (value, unit) => {
+
+    })
 }
 
 function openUnit(unit) {
-    win.header.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff" onclick="goBack();"><path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/></svg><h1>${unit}</h1>`;
-    document.getElementById('win-input').innerHTML = `<input type="text" class="input" placeholder="z. B. ${units[unit].example}" id="unit-input" autocomplete="off"><button class="btn-pink" onclick="unitResults.innerHTML = calculate(document.getElementById('unit-input').value)">Berechnen</button>`;
+    document.getElementById('header-win-title').innerHTML = unit;
+    document.getElementById('win-input').innerHTML = `<input type="text" class="input" placeholder="z. B. ${units[unit].example}" id="unit-input" autocomplete="off"><button class="btn-pink" onclick="unitResults.innerHTML = calculate(document.getElementById('unit-input').value, '${unit}')">Berechnen</button>`;
 
     win.window.style.top = '0%';
     win.window.style.opacity = '1';
@@ -44,15 +64,17 @@ function goBack() {
     win.window.style.opacity = '0';
 }
 
-function calculate(input) {
+function calculate(input, type) {
+    // Check if input valid
     input = input.trim();
     if(input == '' || !/[A-z]/.test(input) || !/[0-9]/.test(input)) {
         return '<div class="error">Eingabe nicht gültig</div>';
     }
     let fistLetterPos = findFirstLetter(input);
+    // parse input in value and unit
     let value = input.slice(0, fistLetterPos);
     let unit = input.slice(fistLetterPos);
-    return value + '\n' + unit;
+    return units[type].calculate(value, unit);
 }
 
 function findFirstLetter(str) {
