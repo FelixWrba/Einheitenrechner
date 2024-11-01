d7c2e03578eb79613079a80fe637f1a68d87eaaa
Felix Wrba <felix.wrba@web.de>
2024-11-01 21:40:23 +0100

    Fixed pwa

--- sw.js
+++ sw.js
@@ -1,37 +1,54 @@
-const CACHE_NAME = 'Einheitenrechner';
+const CACHE_NAME = 'einheitenrechner';
+const URLS_TO_CACHE = [
+    '/index.html',
+    '/imprint.html',
+    '/style.css',
+    '/script.js',
+    '/icons/16.png',
+    '/icons/32.png',
+    '/icons/128.png',
+    '/icons/192.png',
+    '/icons/200.png',
+    '/icons/512.png'
+];
 
-// install event
-self.addEventListener('install', event => {
-    event.waitUntil((async () => {
-        const cache = await caches.open(CACHE_NAME);
-        cache.addAll([
-            '/index.html',
-            '/style.css',
-            '/script.js',
-            '/imprint.html'
-        ]);
-    })());
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Dateien werden im Cache gespeichert');
+                return cache.addAll(URLS_TO_CACHE);
+            })
+    );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith((async () => {
-        const cache = await caches.open(CACHE_NAME);
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Alter Cache wird gelöscht:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
 
-        // Get the resource from the cache.
-        const cachedResponse = await cache.match(event.request);
-        if (cachedResponse) {
-            return cachedResponse;
-        } else {
-            try {
-                // If the resource was not in the cache, try the network.
-                const fetchResponse = await fetch(event.request);
-
-                // Save the resource in the cache and return it.
-                cache.put(event.request, fetchResponse.clone());
-                return fetchResponse;
-            } catch (e) {
-                console.error(`Caught Error: ${e}`);
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) {
+                return response;
             }
-        }
-    })());
+            return fetch(event.request).then((networkResponse) => {
+                return caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, networkResponse.clone());
+                    return networkResponse;
+                });
+            });
+        })
+    );
 });
